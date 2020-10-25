059a40f0ee8e83f21b4ae8e72e1c3053dd341289
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-25 22:06:13 +0000

    Something

--- print_functions.c
+++ print_functions.c
@@ -9,7 +9,7 @@
 int print_char(va_list c)
 {
 	char x = va_arg(c, int);
-
+	
 	write(1, &x, 1);
 	return (1);
 }
@@ -25,6 +25,8 @@ int print_string(va_list s)
 	char *str;
 
 	str = va_arg(s, char*);
+	if (str == NULL)
+		return (0);
 	for (count = 0; str[count]; count++)
 	{
 		write(1, &str[count], 1);
