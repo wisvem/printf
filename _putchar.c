3bcd8d70fb3d3e3fb6a46241190c25cce9d43b67
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-28 04:23:15 +0000

    Fixed _putchar

--- _putchar.c
+++ _putchar.c
@@ -9,18 +9,5 @@
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
