9cc87257deeb351fa63ded5928e49ce3f4269ba2
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-28 03:11:55 +0000

    Added buffer 1024 to putchar

--- _putchar.c
+++ _putchar.c
@@ -9,6 +9,18 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
-}
+	static char buf[1024];
+	static int i;
 
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
